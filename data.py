8d87c6682aa2fe5ba515afef211281dda768ba21
chirag773 <chiragprajapati781@gmail.com>
2019-12-17 00:31:02 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id':1,
+            'author':'Chirag prajapati',
+            'title':'First article',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, labore nesciunt repellat tempora ratione, at quiillo ut sequi ea omnis. Libero accusamus laboriosam reprehenderit tempora perspiciatis aperiam a inventore',
+            'create_date':'21-11-2019'
+        },
+         {
+            'id':2,
+            'author':'Saurabh pandey',
+            'title':'Second article',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, labore nesciunt repellat tempora ratione, at quiillo ut sequi ea omnis. Libero accusamus laboriosam reprehenderit tempora perspiciatis aperiam a inventore',
+            'create_date':'21-11-2019'
+        },
+          {
+            'id':3,
+            'author':'Riya chaturvedhi',
+            'title':'Third article',
+            'body':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, labore nesciunt repellat tempora ratione, at quiillo ut sequi ea omnis. Libero accusamus laboriosam reprehenderit tempora perspiciatis aperiam a inventore',
+            'create_date':'21-11-2019'
+        }
+    ]
+    return articles
